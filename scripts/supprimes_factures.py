9d5b801a59aa160caa0e2cfebbb5e665d0290306
Naily07 <leonelherman22@gmail.com>
2026-06-09 23:21:06 +0300

    maj script suppresion facture

--- scripts/supprimes_factures.py
+++ scripts/supprimes_factures.py
@@ -1,8 +1,17 @@
+import os
+import sys
+import django
+
+# Chemin relatif depuis le dossier où tu lances le script
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pharma.settings')
+django.setup()
 from datetime import datetime
 from django.utils.timezone import make_aware
 from django.db import transaction
 from django.contrib.contenttypes.models import ContentType
-
 from stock.models import Facture, Reglement
 
 date_limite = make_aware(datetime(2026, 5, 1))
